f6b35740536b809e3e661b9c26b1d6f59960ccc5
KhinCho <khinchotint@onenex.co>
2024-11-19 21:28:00 +0630

    Initial Commit

---
+++ WeatherAnimation/WeatherAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAnimationApp.swift
+//  WeatherAnimation
+//
+//  Created by Kcct on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
